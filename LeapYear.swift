c1c430f0b2d41d4abcd2b782b0782c2216be62c1
Alexander Cleoni <30603679+ACleoni@users.noreply.github.com>
2023-03-19 17:55:17 -0400

    Create LeapYear.swift

---
+++ LeapYear.swift
@@ -0,0 +1,32 @@
+class Year {
+    let calendarYear:  Int;
+    var isLeapYear: Bool {
+        guard calendarYear % 4 == 0 else {
+            return false
+        }
+        if calendarYear % 100 == 0 {
+            return calendarYear % 400 == 0
+        }
+        return true  
+    }
+    
+    init(calendarYear: Int) {
+      self.calendarYear = calendarYear;                        
+    }
+}
+
+let vanillaLeapYear = Year(calendarYear: 1996)
+print(centuryIsNotALeapYear.isLeapYear)
+// => true
+
+let notALeapYear = Year(calendarYear: 1997)
+print(notALeapYear.isLeapYear)
+// => false
+
+let centuryIsNotALeapYear = Year(calendarYear: 1900)
+print(centuryIsNotALeapYear.isLeapYear)
+// => false
+
+let centuryIsALeapYear = Year(calendarYear: 2000)
+print(centuryIsALeapYear.isLeapYear)
+// => true
